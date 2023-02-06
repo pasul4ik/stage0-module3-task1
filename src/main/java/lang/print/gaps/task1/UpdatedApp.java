91824ebd42f11cc00144b94d09fe93500a7f0314
PavelDenisov <pavel.den111@gmail.com>
2023-02-06 16:47:12 +0200

    Done

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task1;
 public class UpdatedApp {
     public static void main(String[] args) {
 
-        System.out.println("I am updated");
+        System.out.println("I'm updated");
         System.out.println("can be compiled. And can write to console");
     }
 }
